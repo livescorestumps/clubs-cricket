a03f177ea191f954e1564a0a33b63714cc2bbf0e
Venkareddy <venkareddy.yatham@gmail.com>
2018-10-12 23:14:15 -0400

    created kotlin activity to create new team

---
+++ app/src/main/java/com/game/clubs/stumps/view/NewTeamActivity.kt
@@ -0,0 +1,13 @@
+package com.game.clubs.stumps.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.game.clubs.stumps.R
+
+class NewTeamActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_team)
+    }
+}
